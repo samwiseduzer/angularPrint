723be6c24c522d9fa12187d27068e846a492a7d8
Sam <samwiseduzer@yahoo.com>
2015-03-16 22:52:26 -0600

    manip dom only on click

--- angularPrint.js
+++ angularPrint.js
@@ -2,53 +2,46 @@
 
 (function(){
     var AngularPrint = angular.module('AngularPrint',[]);
-    AngularPrint.directive('printSection',function(){
-        return {
-            restrict: 'A',
-            link: {
-                post: function(scope, element, attr){
-
-                    function printElement(elem) {
-                        var parent = elem.parentNode;
+    AngularPrint.directive('printSection', ['printData', function(printData){
+            return {
+                restrict: 'A',
+                link: {
+                    pre: function(scope, element, attr){
+                        var elem = document.getElementById(attr.printElementId);
+                        elem.classList.add('printShow');
+                        if(attr.addClass){
+                            var classesToAdd = attr.addClass.split(' ');
+                            for(var i = 0; i < classesToAdd.length; i++){
+                                elem.classList.add(classesToAdd[i]);
+                            }
+                        }
+                        if(attr.removeClass){
+                            var classesToRemove = attr.removeClass.split(' ');
+                            for(var j = 0; j < classesToRemove.length; j++){
+                                elem.classList.remove(classesToRemove[j]);
+                            }
+                        }
                         var printSection = document.createElement('div');
-                        printSection.class = 'printSection';
                         printSection.appendChild(elem);
-                        parent.appendChild(printSection);
+                        var parent = elem.parentNode;
+                        parent.id = parent.id + ' ' + printData.assignParentId();
+                        printData.addElem(printSection);
                     }
-
-                    var elem = document.getElementById(attr.printElementId);
-                    elem = elem.cloneNode(true);
-                    elem.classList.add('printShow');
-                    if(attr.addClass){
-                        var classesToAdd = attr.addClass.split(' ');
-                        for(var i = 0; i < classesToAdd.length; i++){
-                            elem.classList.add(classesToAdd[i]);
-                        }
-                    }
-                    if(attr.removeClass){
-                        var classesToRemove = attr.removeClass.split(' ');
-                        for(var j = 0; j < classesToRemove.length; j++){
-                            elem.classList.remove(classesToRemove[j]);
-                        }
-                    }
-                    printElement(elem);
                 }
-            }
-        };
-    });
+            };
+        }]);
     AngularPrint.directive('printBtn',['printData','$window', function(printData,$window){
         return {
             restrict: 'A',
             link: function(scope,element){
 
-                var printSection = document.getElementById('printSection');
-                if(!printSection){
-                    printSection = document.createElement('div');
-                    printSection.id = 'printSection';
-                    document.body.appendChild(printSection);
-                }
-
                 function createTable(){
+                    var AngularPrintTable = document.getElementById('AngularPrintTable');
+                    if(!AngularPrintTable){
+                        AngularPrintTable = document.createElement('div');
+                        AngularPrintTable.id = 'AngularPrintTable';
+                        document.body.appendChild(AngularPrintTable);
+                    }
                     var table = document.createElement('table');
                     var colNames = printData.colNames;
                     var th = document.createElement('th');
@@ -70,15 +63,19 @@
                     }
                     var addClasses = printData.addClasses;
                     table.className = addClasses;
-                    printSection.appendChild(table);
+                    AngularPrintTable.appendChild(table);
                 }
 
                 element.on('click', function(){
                     if(printData.usingTable){
                         createTable();
                     }
+                    for(var i = 1, parent; i <= printData.count; i++){
+                        parent = document.getElementById('AngularPrintParent' + i);
+                        parent.appendChild(printData.elems[i-1]);
+                    }
                     $window.print();
-                    //printSection.innerHTML = '';
+                    document.getElementById('AngularPrintTable').innerHTML = '';
                 });
             }
         };
@@ -132,21 +129,27 @@
         };
     }]);
     AngularPrint.service('printData', function(){
+        this.usingTable = false;
         this.tableData = [];
         this.addClasses = [];
         this.colNames = [];
-        this.usingTable = false;
+        this.count = 0;
+        this.elems = [];
         this.addRow = function(row){
             this.tableData.push(row);
-        };
-        this.clearTable = function(){
-            this.tableData = [];
         };
         this.setAddClasses = function(str){
             this.addClasses = str;
         };
         this.setColNames = function(arr){
             this.colNames = arr;
+        };
+        this.assignParentId = function(){
+            this.count++;
+            return 'AngularPrintParent' + this.count;
+        };
+        this.addElem = function(elem){
+            this.elems.push(elem);
         };
     });
 })();
