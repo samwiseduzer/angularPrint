36f300441470eb1c5670502d70944d7e6f94bdcb
Sam <samwiseduzer@yahoo.com>
2015-03-18 23:11:40 -0600

    add optimizations for printing

--- angularPrint.js
+++ angularPrint.js
@@ -53,6 +53,20 @@
             }
         };
     }]);
+    AngularPrint.directive('printLandscape',function(){
+        return {
+            restrict: 'A',
+            link: function(){
+                var sheet = (function() {
+                    var style = document.createElement('style');
+                    style.appendChild(document.createTextNode(''));
+                    document.head.appendChild(style);
+                    return style.sheet;
+                })();
+                sheet.insertRule('@page{size:landscape;}', 0);
+            }
+        };
+    });
     //attrs: colMap{objProp:{colName:string,addClassess:string}}, data[{}], strictObj(bool), tableClasses(string)
     AngularPrint.directive('apTable', function(){
         return{
@@ -153,21 +167,23 @@
     AngularPrint.directive('printTable', function(){
         return{
             restrict: 'A',
-            scope: {data:'='},
+            scope: {printData:'='},
             link: function(scope, element, attr){
-                function createClone(newVal){
+                function makeTable(newVal){
                     if(newVal == null) return;
                     setTimeout(function(){
-                        var elem = element[0].cloneNode(true);
+                        var elem = (attr.hasOwnProperty('printClone')) ? element[0].cloneNode(true) : element[0];
                         elem.classList.add('printSection');
-                        elem.classList.add('printOnly');
-                        var old = document.getElementById('print-table');
-                        if(old){
-                            old.parentNode.removeChild(old);
+                        if(attr.hasOwnProperty('printClone')){
+                            elem.classList.add('printOnly');
+                            var old = document.getElementById('print-table');
+                            if(old){
+                                old.parentNode.removeChild(old);
+                            }
                         }
                         elem.id = 'print-table';
                         if(attr.addClass){
-                            elem.className += ' ' + attr.addClass;
+                            elem.className += (elem.className) ? ' ' + attr.addClass : attr.addClass;
                         }
                         var tds = elem.getElementsByTagName('td');
                         for(var i = 0, content, div; i < tds.length; i++){
@@ -178,10 +194,12 @@
                             div.innerHTML = content;
                             tds[i].appendChild(div);
                         }
-                        element[0].parentNode.insertBefore(elem,element[0]);
+                        if(attr.hasOwnProperty('printClone')){
+                           element[0].parentNode.insertBefore(elem,element[0]); 
+                        }
                     },1000);
                 }
-                scope.$watchCollection('data', createClone);
+                scope.$watchCollection('printData', makeTable);
             }
         };
     });
